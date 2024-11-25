8d92ae4ff5bfcc3ffab98bb3826e224253869225
Suprith <suprithbadiger2002@gmail.com>
2024-11-25 12:07:41 +0530

    team lead

---
+++ Models/CreateTeam.cs
@@ -0,0 +1,15 @@
+﻿namespace weekday.Models
+{
+    public class CreateTeam
+    {
+        public int TeamId { get; set; }
+
+        public string TeamName { get; set; }
+        public int ProjectId { get; set; }
+
+        public string ProjectName { get; set; }
+
+        public List<int> MembersId { get; set; }
+
+    }
+}
