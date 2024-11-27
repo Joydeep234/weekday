f7b9faaf53ec875ca96c90178921cb9686b42baa
Suprith <suprithbadiger2002@gmail.com>
2024-11-27 17:51:32 +0530

    task feature updates

---
+++ Models/CreateTask.cs
@@ -0,0 +1,24 @@
+﻿namespace weekday.Models
+{
+    public class CreateTask
+    {
+        public int ProjectId { get; set; }
+
+        public int TeamId { get; set; }
+
+        public int TaskType { get; set; }
+
+        public string Priority { get; set; }
+
+        public string Summary { get; set; }
+
+        public string Status { get; set; }
+
+        public string Description { get; set; } 
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime Deadline { get; set; }
+       
+    }
+}
--- Models/CreateTeam.cs
+++ Models/CreateTeam.cs
@@ -1,14 +1,24 @@
-﻿namespace weekday.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace weekday.Models
 {
     public class CreateTeam
     {
+        [Required(ErrorMessage = "Please select a team")]
+        [Range(1, int.MaxValue, ErrorMessage = "Please select a valid team." )]
         public int TeamId { get; set; }
 
-        public string TeamName { get; set; }
+        //public string TeamName { get; set; }
+
+        [Required(ErrorMessage = "Please select a project.")]
+        [Range(1, int.MaxValue, ErrorMessage = "Please select a valid project.")]
         public int ProjectId { get; set; }
 
-        public string ProjectName { get; set; }
+        //public string ProjectName { get; set; }
 
+        [Required(ErrorMessage = "Please select at least one team member.")]
+        [MinLength(1, ErrorMessage = "Please select at least one team member.")]
         public List<int> MembersId { get; set; }
 
     }
