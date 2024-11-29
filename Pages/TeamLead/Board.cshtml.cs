14ce9c075a319d3f459987ab08a04302aa9586d3
Suprith <suprithbadiger2002@gmail.com>
2024-11-29 17:50:38 +0530

    board updates

--- Pages/TeamLead/Board.cshtml.cs
+++ Pages/TeamLead/Board.cshtml.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Diagnostics.Contracts;
+using System.Security.Cryptography.Xml;
 using weekday.Data.Context;
 using weekday.Data.Entity;
 using weekday.Models;
@@ -26,10 +29,18 @@ namespace weekday.Pages.TeamLead
 
         public List<Employee> employees { get; set; } = new List<Employee>();
 
+        public List<ProjectTeamMembers> projectTeamMembers { get; set; } = new List<ProjectTeamMembers>(); 
         public List<Team> teamList { get; set; } = new List<Team>();
+
+        public List<ProjectTask> projectTasks { get; set; }
+
+        public int randomDigitNonZero;
 
         public IActionResult OnGet()
         {
+            Random random = new Random();
+            randomDigitNonZero = random.Next(1, 10);
+
             projectDetails = (from p in _context.project
                               join t in _context.team on p.ProjectId equals t.ProjectId
                               join tM in _context.teamMembers on t.TeamId equals tM.TeamId
@@ -60,10 +71,44 @@ namespace weekday.Pages.TeamLead
                          where D.Name == "TEAM_MEMBER"
                          select E)
                          .ToList();
+
+
+            projectTasks = (from T in _context.projecttask
+                            where T.AssignedById == 3
+                            select T).ToList();
+
             //TempData["designationId"] = employees[0].DesignationId;
             TempData["designationId"] = employees.FirstOrDefault()?.DesignationId ?? 0;
 
             return Page();
+        }
+
+        public async Task<IActionResult> OnGetProjectTeamMembers(int ProjectId, int TeamId)
+        {
+
+            
+            projectTeamMembers = await (from T in _context.team
+                                        join Tm in _context.teamMembers on T.TeamId equals Tm.TeamId
+                                        join E in _context.employee on Tm.MemberId equals E.EmployeeId
+                                        join D in _context.designation on E.DesignationId equals D.DesignationId
+                                        where T.ProjectId == ProjectId && T.TeamId == TeamId && D.Name == "TEAM_MEMBER"
+                                        select new ProjectTeamMembers
+                                        {
+                                            EmployeeId = E.EmployeeId,
+                                            FirstName = E.FirstName,
+                                            LastName = E.LastName,
+                                            ImageURL = E.ImageURL,
+                                        }).ToListAsync();
+
+            
+            if(projectTeamMembers != null)
+            {
+                return new JsonResult(new { success = true, teamMembers = projectTeamMembers });
+            }
+            else
+            {
+                return new JsonResult(new { success = false });
+            }
         }
 
         public async Task<JsonResult> OnGetTeamList(int ProjectId)
@@ -79,7 +124,6 @@ namespace weekday.Pages.TeamLead
         [ValidateAntiForgeryToken]
         public IActionResult OnPostCreateOrUpdate([FromBody] CreateTeam team)
         {
-            
                 List<TeamMembers> newTeamMembers = new List<TeamMembers>();
                 foreach (var memberId in team.MembersId)
                 {
@@ -115,6 +159,66 @@ namespace weekday.Pages.TeamLead
             
             return new JsonResult(new { success = false });
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> OnPostCreateOrUpdateTask([FromBody] CreateTask taskData)
+        {  
+            if(taskData != null)
+            {
+                ProjectTask task = new ProjectTask
+                {
+                    ProjectId = taskData.ProjectId,
+                    TeamId = taskData.ProjectId,
+                    Title = taskData.TaskType,
+                    Summary = taskData.Summary,
+                    Details = taskData.Description,
+                    Status = taskData.Status,
+                    Priority = taskData.Priority,
+                    AssignedById = 3, // TL id from session
+                    AssignedForId = taskData.Assignee,
+                    AssignedDate = DateTime.Now,
+                    StartDate = taskData.StartDate,
+                    Deadline = taskData.Deadline,
+                    OrgId = 2
+                };
+
+                await _context.projecttask.AddAsync(task);
+                await _context.SaveChangesAsync();
+
+                return new JsonResult(new { success = true });
+            }
+            else
+            {
+                return new JsonResult(new { success = false });
+            }   
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult OnPostUpdateTaskOrder(string fromColumn, string toColumn, List<int> taskIds)
+        {
+            try
+            {
+                var tasks = _context.projecttask.Where(t => taskIds.Contains(t.TaskId)).ToList();
+
+                foreach (var task in tasks)
+                {
+                    // Update the task's column and order in the database
+                    task.Status = toColumn; // Set to the new column
+                    //task.Order = taskIds.IndexOf(task.Id); // Set the new order based on position
+                }
+
+                _context.SaveChanges();
+                return new JsonResult(new { success = true });
+
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(new { success = false, message = ex.Message });
+            }
+           
+        }
     }
 
     public class ProjectDetails
@@ -134,32 +238,21 @@ namespace weekday.Pages.TeamLead
         public string ManagerLastName { get; set; }
         public string ManagerEmail { get; set; }
     }
+
+    public class ProjectTeamMembers
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }   
+
+        public string ImageURL { get; set; }
+
+
+    }
 }
 
 
 
-/*[HttpPost]
-public IActionResult UpdateTaskOrder(string fromColumn, string toColumn, List<int> taskIds)
-{
-    try
-    {
-        // Assuming you have a Task entity and it has a Column and Order field
-        var tasks = _context.Tasks.Where(t => taskIds.Contains(t.Id)).ToList();
 
-        foreach (var task in tasks)
-        {
-            // Update the task's column and order in the database
-            task.Column = toColumn; // Set to the new column
-            task.Order = taskIds.IndexOf(task.Id); // Set the new order based on position
-        }
-
-        _context.SaveChanges();
-
-        return Json(new { success = true });
-    }
-    catch (Exception ex)
-    {
-        return Json(new { success = false, message = ex.Message });
-    }
-}*/
 
